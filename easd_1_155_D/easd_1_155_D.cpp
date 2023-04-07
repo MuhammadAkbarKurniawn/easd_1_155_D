7308565dee57c4bdff196c759bf94f3e69853fb6
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2023-04-07 10:15:23 +0700

    Membuat prosedur input dengan jumlah data maksimal 75

--- easd_1_155_D/easd_1_155_D.cpp
+++ easd_1_155_D/easd_1_155_D.cpp
@@ -3,3 +3,14 @@ using namespace std;
 
 int Akbar[75];
 int n;
+
+void input() {
+	while (true) {
+		cout << "Masukkan Panjang Elemenet Array : ";
+		cin >> n;
+
+		if (n <= 75) break;
+		else cout << "\n Maksimum Panjang Array Adalah 75" << endl;
+	}
+
+}
